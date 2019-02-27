4c4df9c20682ec54c038210bb5e84ca5a4d3fa2d
jagatmachines <jagatjyoti@fusemachines.com>
2019-02-27 11:24:51 +0545

    toggle completed

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ $( document ).ready(function() {
     $(".hamburger").click(function(){
         $(this).toggleClass( 'is-active' );
         $(this).siblings().toggleClass( 'active' );
-        $(this).parent().siblings().toggleClass( 'active' );
+        // debugger;
+        $(this).parent().siblings().toggleClass('removeActive');
+
+        // $(this).parent().siblings().toggleClass( 'active' );
+        // $(this).parent().siblings().css('opacity', 0);
     });
+
+    $("#clickHome").click(() => {
+        $(document).find('.active').removeClass('active removeActive');
+        $(document).find('.is-active').removeClass('is-active');
+        $('#home').addClass('active');
+    })
 });
+
 
